979909d7dceb4e56ef3107cc3089fd35742d0d17
Inseo Yang <y.1nseooo@gmail.com>
2023-04-05 12:16:32 +0900

    chore: 기본 파일 구조 수정 및 추가 (#4 close)
    Co-authored-by: jihyun Yun <jihyun-Yun42@users.noreply.github.com>

---
+++ src/data/type/d3.ts
@@ -0,0 +1,11 @@
+// 엥인서 모듈 파일
+
+export type LoginInfo = {
+  id: string;
+  password: string;
+};
+
+export type CookieOption = {
+  path: string;
+  maxAge?: number;
+};
---
+++ src/lib/api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { getCookie } from "../utils/cookies";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+  // timeout: 1,
+  // 오류 확인 가능한지 테스트.. 1밀리세컨드.. 내에 응답을 못받으면 에러처리 하도록 돼 있음.
+  // 쿼리에서 처리하기
+});
+
+instance.interceptors.request.use(
+  // 요청을 보내기 전 수행되는 함수
+  function (config) {
+    const token = getCookie("token");
+    config.headers["Authorization"] = `${token}`;
+    return config;
+  }
+);
+
+instance.interceptors.response.use(
+  // 응답을 내보내기 전 수행되는 함수
+  function (response) {
+    return response;
+  },
+
+  // 오류 응답을 내보내기 전 수행되는 함수
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
