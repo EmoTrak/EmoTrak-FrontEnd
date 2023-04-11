be07e5c24ae3ee48351c6b7803a7b6b1fffeb194
Inseo Yang <y.1nseooo@gmail.com>
2023-04-11 13:38:05 +0900

    fix: useQuery로 서버통신 수정  #63

--- src/pages/RedirectKakao.tsx
+++ src/pages/RedirectKakao.tsx
@@ -1,39 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { setCookie } from "../utils/cookies";
+import { getCookie, setCookie } from "../utils/cookies";
 import guest from "../lib/api/guest";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { keys } from "../data/queryKeys/keys";
 
 const RedirectKakao = () => {
   const navigate = useNavigate();
   const code: string | null = new URL(window.location.href).searchParams.get(
     "code"
   );
+  const token = getCookie("token");
 
-  const authKakaoCode = useMutation(
-    async (code: string | null) => {
-      const data = await guest.get(`/kakao/callback?code=${code}`);
-      return data;
-    },
-    {
-      onSuccess(data) {
-        const info = data.headers.authorization.split(" ");
-        const token = info[1];
-        setCookie("token", token, { path: "/", maxAge: 3540 });
-        setCookie("nickname", data.headers.nickname, {
-          path: "/",
-          maxAge: 3540,
-        });
-        navigate("/");
-      },
-      onError(err) {},
-    }
-  );
-
-  useEffect(() => {
-    authKakaoCode.mutate(code);
+  const getKakaoLogin = useCallback(async () => {
+    return await guest.get(`/kakao/callback?code=${code}`);
   }, []);
 
+  const authKakaoCode = useQuery([`${keys.GET_KAKAO_LOGIN}`], getKakaoLogin, {
+    retry: 1,
+    onSuccess(data) {
+      const info = data.headers.authorization.split(" ");
+      const token = info[1];
+      setCookie("token", token, { path: "/", maxAge: 3540 });
+      setCookie("nickname", data.headers.nickname, {
+        path: "/",
+        maxAge: 3540,
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
+
+  if (authKakaoCode.isLoading) {
+    return <div>카카오 로그인중입니다...</div>;
+  }
   return <div>RedirectKakao</div>;
 };
 
