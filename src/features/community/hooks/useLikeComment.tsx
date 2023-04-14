07824eb55b6ce0407a2c32f93653b0064998a37d
jihyun Yun <jyun9842@gmail.com>
2023-04-14 21:39:08 +0900

    style: console.log지움 #128

--- src/features/community/hooks/useLikeComment.tsx
+++ src/features/community/hooks/useLikeComment.tsx
@@ -4,7 +4,6 @@ import user from "../../../lib/api/user";
 const useLikeComment = (setLike: React.Dispatch<React.SetStateAction<object>>) => {
   const { mutate } = useMutation({
     mutationFn: async (id: number | undefined) => {
-      console.log(id);
       const data = await user.post(`/boards/comments/likes/${id}`);
       return data.data;
     },
