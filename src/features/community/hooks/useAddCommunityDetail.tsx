22ea485338406defb1d7295a4ab312d5b7e6cf08
jihyun Yun <jyun9842@gmail.com>
2023-04-16 20:58:05 +0900

    fix: 좋아요기능 오류해결 #153

--- src/features/community/hooks/useAddCommunityDetail.tsx
+++ src/features/community/hooks/useAddCommunityDetail.tsx
@@ -8,7 +8,7 @@ const useAddCommunityDetail = (page: number) => {
   const param = useParams();
   const navigate = useNavigate();
   const dailyId: number = Number(param.id);
-  const { data, isLoading, status } = useQuery({
+  const { data, isLoading, status, remove } = useQuery({
     queryKey: [keys.GET_BOARD, page],
     queryFn: async () => {
       const data = await user.get(`/boards/${dailyId}`, { params: { page: page } });
@@ -18,9 +18,10 @@ const useAddCommunityDetail = (page: number) => {
       error?.response?.status === 404 && alert("삭제된 페이지입니다");
       navigate(-1);
     },
+    // refetchOnMount: "always",
   });
 
-  return { data, isLoading, status };
+  return { data, isLoading, status, remove };
 };
 
 export default useAddCommunityDetail;
