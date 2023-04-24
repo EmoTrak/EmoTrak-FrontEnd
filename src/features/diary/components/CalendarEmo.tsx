faa30ff3c99258e0f9a60e746ee12fb0b8f5912c
jihyun Yun <jyun9842@gmail.com>
2023-04-24 21:47:32 +0900

    design: calendar 반응형웹 디자인 #280

--- src/features/diary/components/CalendarEmo.tsx
+++ src/features/diary/components/CalendarEmo.tsx
@@ -1,6 +1,7 @@
 import { DayProps } from "../../../data/type/d1";
 import EmotionIcons from "../../../components/Icon/EmoticonIcons";
 import styled from "styled-components";
+import { device } from "../../../utils/theme";
 
 const CalendarEmo = ({ data, item }: Partial<DayProps>) => {
   const emoDate = data?.contents.find((e) => e.day === item?.date);
@@ -22,11 +23,16 @@ const CalendarEmo = ({ data, item }: Partial<DayProps>) => {
 
 const Imoticon = styled.div`
   border-radius: 50%;
-  width: 5vw;
+  width: 70%;
   margin-top: 10px;
-  margin-left: 13px;
+  margin-left: 15%;
   position: absolute;
   z-index: 0;
+  ${device.mobile} {
+    margin-top: 13px;
+    margin-left: 11%;
+    width: 70%;
+  }
 `;
 
 export default CalendarEmo;
--- src/features/diary/components/MiniCalendar.tsx
+++ src/features/diary/components/MiniCalendar.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { date } from "../../../data/type/d1";
-import { themeColor } from "../../../utils/theme";
+import { device, themeColor } from "../../../utils/theme";
 
 const MiniCalendar = ({ year, month }: date) => {
   if (month === 13) {
@@ -55,6 +55,9 @@ const MiniCalendar = ({ year, month }: date) => {
 const CalendarBox = styled.div`
   width: 200px;
   margin: 50px 0 0 3vw;
+  ${device.tablet} {
+    display: none;
+  }
 `;
 
 const DiaryDay = styled.div`
