f8c88a382116df72541798dffbfb11b30b633a61
Inseo Yang <y.1nseooo@gmail.com>
2023-05-01 02:42:01 +0900

    fix: pwa 설치 알림 버그-3 #427

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import LoginForm from "../features/login/components/LoginForm";
 import Tutorial from "../features/login/components/Tutorial";
 import Landing from "../features/login/components/Landing";
@@ -6,43 +6,15 @@ import { LoginPageWrapper } from "../features/login/styles/LoginFormStyle";
 import { useWindowSize } from "../hooks/useWindowSize";
 
 const Login = () => {
-  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
-
   const { resizeHandler, desktop } = useWindowSize();
 
   useEffect(() => {
     window.addEventListener("resize", resizeHandler);
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
     return () => {
       window.removeEventListener("resize", resizeHandler);
-      window.removeEventListener(
-        "beforeinstallprompt",
-        handleBeforeInstallPrompt
-      );
     };
   }, []);
-
-  useEffect(() => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-
-      deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
-        if (choiceResult.outcome === "accepted") {
-          alert("매일 감정을 기록해보세요!");
-        } else {
-          console.log("");
-        }
-
-        setDeferredPrompt(null);
-      });
-    }
-  }, [deferredPrompt]);
-
-  const handleBeforeInstallPrompt = (event: Event) => {
-    event.preventDefault();
-    setDeferredPrompt(event);
-  };
 
   return (
     <LoginPageWrapper>
