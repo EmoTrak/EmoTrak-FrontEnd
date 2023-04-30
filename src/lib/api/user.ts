92365968d32035516e0b0c7333c3625896011e17
jihyun Yun <jyun9842@gmail.com>
2023-04-30 14:09:53 +0900

    fix: 에러코드 수정 #376

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
 import { getCookie } from "../../utils/cookies";
 import { useError } from "../../hooks/useError";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 const user = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 user.interceptors.request.use(
-  function (config) {
+  function (config: InternalAxiosRequestConfig) {
     const token = getCookie("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
